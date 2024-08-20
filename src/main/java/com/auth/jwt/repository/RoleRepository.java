876be46563f4b5fa006a4b74d3a059b0970b229d
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-20 23:16:09 +0300

    add: repository

--- src/main/java/com/auth/jwt/repository/RoleRepository.java
+++ src/main/java/com/auth/jwt/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package com.auth.jwt.repository;
 import com.auth.jwt.domain.enums.RoleEnum;
 import com.auth.jwt.domain.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Boolean existsByName(RoleEnum name);
     Role findByName(RoleEnum name);
--- src/main/java/com/auth/jwt/repository/UserRepository.java
+++ src/main/java/com/auth/jwt/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.auth.jwt.repository;
 
 import com.auth.jwt.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
 
