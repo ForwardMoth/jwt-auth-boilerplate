35a8e6e04680fb2279586570d1b0840d95dcc34d
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-20 18:05:00 +0300

    fix: some bugs

--- src/main/java/com/auth/jwt/config/security/SecurityConfiguration.java
+++ src/main/java/com/auth/jwt/config/security/SecurityConfiguration.java
@@ -7,8 +7,10 @@ import com.auth.jwt.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -70,5 +72,11 @@ public class SecurityConfiguration {
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
         return corsConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/auth/jwt/controller/superadmin/SuperAdminController.java
+++ src/main/java/com/auth/jwt/controller/superadmin/SuperAdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/super-admin")
 public class SuperAdminController {
     @GetMapping
     public String get() {
