de8d1f49cc38fe266e3ef2176a3947f473bb583e
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-20 17:51:15 +0300

    add: auth controller

---
+++ src/main/java/com/auth/jwt/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.auth.jwt.controller;
+
+import com.auth.jwt.domain.dto.request.SignInRequest;
+import com.auth.jwt.domain.dto.request.SignUpRequest;
+import com.auth.jwt.domain.dto.response.JwtAuthResponse;
+import com.auth.jwt.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthResponse> signUp(@RequestBody SignUpRequest request){
+        return ResponseEntity.ok(authService.signUp(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthResponse> signIn(@RequestBody SignInRequest request){
+        return ResponseEntity.ok(authService.signIn(request));
+    }
+}
